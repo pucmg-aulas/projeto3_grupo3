d9a78003f58c0105f0ed03bf331727ef5645d13c
Lucas Ribeiro <64383585+LeaRN-02@users.noreply.github.com>
2023-12-03 20:54:55 -0300

    Add files via upload

---
+++ src/parking/archive/ClienteArchive.java
@@ -0,0 +1,61 @@
+package parking.archive;
+
+import parking.model.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ClienteArchive extends AbstractArchive implements Serializable {
+
+    private List<Cliente> clientes;
+    private static ClienteArchive clienteArchive;
+    private final String filePath = "./src/parking/data/Cliente.dat";
+
+    private ClienteArchive() {
+        clientes = new ArrayList<>();
+        carregaClientes();
+    }
+
+    public static ClienteArchive getInstance() {
+        if (clienteArchive == null) {
+            clienteArchive = new ClienteArchive();
+        }
+        return clienteArchive;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    private void carregaClientes() {
+        clientes = fileRead(filePath);
+    }
+
+    public void addCliente(Cliente cliente) {
+        veiculos.add(veiculo);
+        fileWrite(filePath, veiculos);
+    }
+
+    public void removeCliente(Cliente cliente) {
+        clientes.remove(cliente);
+        fileWrite(filePath, clientes);
+    }
+
+    public void editarClientes(Cliente clienteNovo, Cliente clienteAntigo) {
+        clientes.set(clientes.indexOf(clienteAntigo), clienteNovo);
+        fileWrite(filePath, clientes);
+    }
+
+    public Cliente buscarClientePorId(String id) {
+        Iterator<Cliente> iterator = clientes.iterator();
+        while (iterator.hasNext()) {
+            Cliente cliente = iterator.next();
+            if (cliente.getId().equals(id)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+}
